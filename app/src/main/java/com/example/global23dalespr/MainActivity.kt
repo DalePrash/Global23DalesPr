628e8ddcf877b2ef946ecbcb1b1f427ee431de2e
DALESH PRASHAR <dalesh.prash@gmail.com>
2022-11-23 11:30:57 -0600

    First Commit

---
+++ app/src/main/java/com/example/global23dalespr/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.global23dalespr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    // check
+}
